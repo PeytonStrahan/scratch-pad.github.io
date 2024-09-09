6d74bdfc6c087a649db5b4932f5aac5a743f593a
PeytonStrahan <peytonstrahan@outlook.com>
2024-09-09 16:04:44 -0500

    day 1 halfway through stringy

--- day-1/stringy.js
+++ day-1/stringy.js
@@ -33,9 +33,21 @@ function length(string) {
 /**
  * Given an input String, return a new String forced to lowercase.
  */
+
+/*
+I: Function takes in a string
+O: Return a new string that is the lowercased version of the input string
+C: N/A
+E: N/A
+*/
+
 function toLowerCase(string) {
     // YOUR CODE BELOW HERE //
    
+    // create new string assigned to the result of using the .toLowerCase() method on the input string
+    var newString = string.toLowerCase();
+    // return the new string
+    return newString;
 
     // YOUR CODE ABOVE HERE //
 }
@@ -43,10 +55,21 @@ function toLowerCase(string) {
 /**
  * Given an input String, return a new String forced to uppercase.
  */
+
+/*
+I: Function takes in a string
+O: Return a new string that is the uppercased version of the input string
+C: N/A
+E: N/A
+*/
+
 function toUpperCase(string) {
     // YOUR CODE BELOW HERE //
 
-
+    // create new string assigned to the result of using the .toUpperCase() method on the input string
+    var newString = string.toUpperCase();
+    // return the new string
+    return newString;
 
     // YOUR CODE ABOVE HERE //
 }
@@ -64,10 +87,23 @@ function toUpperCase(string) {
  *
  *      See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Using_global_and_ignore_with_replace()
  */
+
+/*
+I: Function takes in a string
+O: Return a new string that has had its spaces (" ") replaces with dashes ("-") and is the lowercased version of the input string
+C: N/A
+E: N/A
+*/
+
 function toDashCase(string) {
     // YOUR CODE BELOW HERE //
 
-
+    // create new string assigned to the result of using the .toLowerCase() method on the input string
+    var newString = string.toLowerCase();
+    // replace all spaces in the new string with dashes
+    newString = newString.replaceAll(" ", "-");
+    // return the new string
+    return newString;
 
     // YOUR CODE ABOVE HERE //
 }
@@ -84,10 +120,29 @@ function toDashCase(string) {
  * TIP: How can you use Array access to your advantage here? How can you
  *      ensure uppercase and lowercase can be compared equally?
  */
+
+/*
+I: Function takes in a string and a single character (presumably a letter)
+O: Return a boolean that says true if the single character and the first character of the string are the same character, but returns false otherwise
+C: N/A
+E: Whether the character is uppercase or lowercase should not affect the output of the function
+*/
+
 function beginsWith(string, char) {
     // YOUR CODE BELOW HERE //
 
-    
+    //make both the input string and input char lowercase
+    string = string.toLowerCase();
+    char = char.toLowerCase();
+
+    //determine if char and the first character of string are equal
+    if (string[0] === char){
+        //return true if so
+        return true;
+    }
+
+    //return false otherwise
+    return false;
 
     // YOUR CODE ABOVE HERE //
 }
