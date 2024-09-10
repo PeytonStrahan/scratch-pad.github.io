671563c6a07bae9066f23be43970f8576f43543d
PeytonStrahan <peytonstrahan@outlook.com>
2024-09-10 15:14:44 -0500

    day 2 homework done

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -10,10 +10,39 @@
  *  c. AND for the multiples of five print “Buzz”.
  *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
 */
+
+/*
+I: N/A
+O: Console log the numbers 1-100, but each multiple of 3 is replaced with the string "Fizz" and each multiple of 5 is replaced with the string "Buzz"
+C: N/A
+E: Console log "FizzBuzz" for numbers which are multiples of both three and five
+*/
+
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
 
-
+    // create a for loop to loop through numbers 1 to 100
+    for (let i = 1; i <= 100; i++){
+        // determine if current number is evenly divisible by both 3 and 5
+        if (i % 3 == 0 && i % 5 == 0){
+            // print "FizzBuzz" if so
+            console.log("FizzBuzz");
+        }
+        // otherwise, determine if current number is evenly divisible by 3
+        else if (i % 3 == 0){
+            // print "Fizz" if so
+            console.log("Fizz");
+        }
+        // otherwise, determine if current number is evenly divisible by 5
+        else if (i % 5 == 0){
+            // print "Buzz" if so
+            console.log("Buzz");
+        }
+        // otherwise, just print the current number
+        else {
+            console.log(i);
+        }
+    }
 
     // YOUR CODE GOES ABOVE HERE //
 }
