e5599969b26b3e49ef9618b92db3fa3f0e8bc4d1
PeytonStrahan <peytonstrahan@outlook.com>
2024-09-14 23:52:41 -0500

    day 4 contact-list.js done

--- day-4/contact-list.js
+++ day-4/contact-list.js
@@ -33,9 +33,28 @@
  *          new-line character added after it!
  */
 
+/* 1.
+I: Function takes in three properties
+O: Returns a contact object
+C: N/A
+E: N/A
+*/
+
+/* 2.
+I: N/A
+O: Returns an object that manages contact objects
+C: N/A
+E: N/A
+*/
+
 // YOUR CODE GOES BELOW HERE //
 function makeContact(id, nameFirst, nameLast) {
-    
+    // return an unnamed object holding keys with each of this function's inputs assigned as properties
+    return {
+        id: id,
+        nameFirst: nameFirst,
+        nameLast: nameLast
+    };
 } 
 
 
@@ -48,6 +67,56 @@ function makeContactList() {
     
     return {
         // we implemented the length api for you //
+        length: function() {
+            // return the length of the contacts array
+            return contacts.length;
+        },
+        addContact: function(contact) {
+            // adds the inputted contact object to the contacts array
+            contacts.push(contact);
+        },
+        findContact: function(fullName) {
+            // split the inputted fullName into the first and last names in an array and assign said array to a variable
+            var firstAndLast = fullName.split(" ");
+
+            // loop through the contacts array
+            for (let i = 0; i < contacts.length; i++){
+                // determine if the first and last names of the current contact matches the inputted first and last names
+                if (firstAndLast[0] === contacts[i].nameFirst && firstAndLast[1] === contacts[i].nameLast){
+                    // return the current contact if so
+                    return contacts[i];
+                }
+            }
+            // return undefined if the loop finds no matches (this function can technically do it without this line of code due to how functions work, but whatever)
+            return undefined;
+        },
+        removeContact: function(contact) {
+            // loop through the contacts array
+            for (let i = 0; i < contacts.length; i++){
+                // determine if the id of the current contact matches the id of the inputted contact
+                if (contact.id === contacts[i].id){
+                    // remove the current contact if so
+                    contacts.splice(i, 1);
+                }
+            }
+        },
+        printAllContactNames: function() {
+            // create an empty string
+            var allNames = "";
+
+            // loop through the contacts array
+            for (let i = 0; i < contacts.length; i++){
+                // add the current contact's first and last names onto the allNames string
+                allNames = allNames + contacts[i].nameFirst + " " + contacts[i].nameLast;
+                // determine if the current contact is the last in the contacts list
+                if (i === contacts.length - 1) {
+                    // return the allNames string if so
+                    return allNames;
+                }
+                // otherwise, add a line-break and let for loop repeat
+                allNames = allNames + "\n";
+            }
+        }
        
     }
 }
